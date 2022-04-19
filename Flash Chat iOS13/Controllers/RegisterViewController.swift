8fe3bd61048c31a2a66b45ea2654677789f244dc
Sancara <santiagocaraballo@gmail.com>
2022-04-19 07:33:25 -0300

    UI: Segues and Navigation Controller

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -14,6 +14,7 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
     }
     
 }
